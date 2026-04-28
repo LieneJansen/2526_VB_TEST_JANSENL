7f315cd17fd13094e180d1f65c58ace3f14e63b1
LieneJansen <liene.jansen1@gmail.com>
2026-04-28 21:57:31 +0200

    Add files via upload

---
+++ app/Page1/page.tsx
@@ -0,0 +1,68 @@
+export default function AboutPage() {
+  return (
+    <div className="max-w-4xl mx-auto space-y-6">
+      <h1 className="text-3xl font-bold">Leidt AI-slop tot de dood van sociale media?</h1>
+      <h1 className="text-xl font-semibold">    Sociale media slibben de jongste paar maanden angstwekkend snel
+dicht met AI-slop: video’s met mensen die niet bestaan en
+gebeurtenissen die zich niet hebben voorgedaan. Blijven we daar in
+2026 naar kijken?</h1>
+      <p className="text-700">
+   De grootste technologische doorbraak van 2025, of toch die met de zichtbaarste gevolgen,
+zijn de videogenerators als Sora en Veo, waardoor iedereen nu in enkele seconden een
+realistisch uitziende video kan maken over elk onderwerp. De grote vraag voor 2026 is:
+willen we daarnaar blijven kijken?
+      </p>
+      <p>
+Dat we zoveel AI-slop te zien krijgen de jongste weken, heeft niet alleen te maken met de
+enorm toegenomen productie, maar ook met wat algoritmes ons aanbevelen. Met keuzes
+die bedrijven als Meta, Google en Bytedance (Tiktok) voor ons maken, dus. Als je op
+Youtube een nieuwe account aanmaakt, dan is 20 procent van de video’s die je worden
+aangeboden van AI afkomstig.</p>
+
+<p>Mark Zuckerberg heeft het in oktober letterlijk gezegd: AI-gegenereerde inhoud betekent
+een nieuw tijdperk in sociale media. Tijdens het eerste tijdperk deelden we ons eigen
+leven op Facebook en Instagram, zei hij. Maar al snel bleek dat we ons liever vergapen aan
+de levens van celebrity’s en influencers – tijdperk twee. En nu komt er een stortvloed van
+AI-video’s aan.</p>
+<p>Zuckerberg stelt het voor alsof die AI-inhoud er gewoon bovenop komt. Maar laten we wel
+wezen: hoe meer AI in onze feed, hoe minder video’s we bekijken van professionele
+makers en van onze eigen vrienden. Nochtans hadden sociale media echt wel een
+bestaansreden, namelijk dat we geïnteresseerd zijn in wat er gebeurt met echte mensen.
+Zijn we in 2026 dan zo veranderd?</p>
+<p>Volgens Zuckerberg wel. Hij denkt dat dit is wat we écht willen: boven op de algoritmes die
+ons altijd precies tonen wat we graag zien, komen nu de videogenerators die daar zonder
+enige beperking eindeloos meer van kunnen genereren. Nog meer kattenvideo’s,
+onmogelijke stunts, spectaculaire taarten of gewelddadige pranks, of wat ook je ding mag
+zijn (elders op het internet krijgen pornoliefhebbers nu video’s te zien die almaar meer
+tonen van wat ze willen, ook als dat anatomisch totaal onmogelijk is).</p>
+<p>Die beelden zijn niet echt. Die mensen bestaan niet, die dingen zijn nooit gebeurd. Maar
+Zuckerberg gaat ervan uit dat dat er niet toe doet: u wilt kattenvideo’s, u krijgt
+kattenvideo’s die nog veel schattiger zijn dan in het echt.</p>
+<p>Misschien. Maar zelf ben ik de afgelopen twee maanden uiterst gevoelig geworden voor
+AI-slop: vanaf het moment dat ik de indruk krijg dat een video AI-gegenereerd is, klik ik
+’m weg. Net zoals ik stop met een tekst te lezen zodra ik vermoed dat ChatGPT de
+voornaamste auteur was.</p>
+<p>De eerste paar weken was AI-video nieuw en verrassend, dat wel. En nu nog is het soms
+lachen geblazen met sommige van die Sora-filmpjes (ook al mijd ik ze). Er is heus wel een
+plaats voor AI-video, bijvoorbeeld om speciale effecten te produceren in films. En er zijn
+creatieve mensen die toffe, vaak heel grappige video’s maken die zonder AI onbetaalbaar
+duur zouden zijn.</p>
+<p>Maar horen ze thuis op Instagram en Facebook? Dat waren toch ‘sociale’ media, nietwaar?
+Wat precies is de sociale dimensie van kijken naar een eindeloze stroom van door een
+computer gegenereerde video’s, aanbevolen door een algoritme? In die zin zien we nu de
+dood van sociale media. Al lijkt Zuckerberg daar niet echt van wakker te liggen.</p>
+<p>Technocraat Dominique Deckmyn is elke vrijdag te horen in de podcast ‘Bits & atomen’
+.</p>
+
+
+      <ul className="list-disc pl-8 text-gray-700 space-y-2">
+LEES OOK
+<ul className="list-disc pl-5 text-gray-700 space-y-2"></ul>
+<li>Kandidaat-product van het jaar: de AI-videogenerator.
+“De acteursfilm
+zal duurder worden dan de superheldenproductie”</li>
+       
+      </ul>
+    </div>
+  )
+}
---
+++ app/Page3/page.tsx
@@ -0,0 +1,68 @@
+export default function AboutPage() {
+  return (
+    <div className="max-w-4xl mx-auto space-y-6">
+      <h1 className="text-3xl font-bold">Was het verzet van Dario Amodei tegen
+minister Hegseth het “Ik ben Spartacus!”
+-
+moment van Silicon Valley?</h1>
+      <h1 className="text-xl font-semibold">Het verzet van Dario Amodei tegen minister van “Oorlog” Pete Hegseth,
+en de rechtszaak waar dat nu op uitdraait, lijkt steeds meer op een
+keerpunt. Al keert Silicon Valley nooit meer terug naar vroeger.</h1>
+
+      <p className="text-700">
+  HHet verzet van Dario Amodei tegen minister van “Oorlog” Pete Hegseth,
+en de rechtszaak waar dat nu op uitdraait, lijkt steeds meer op een
+keerpunt. Al keert Silicon Valley nooit meer terug naar vroeger.
+      </p>
+      <p>
+Toch staat er rake taal in. De strafmaatregel die minister van “Oorlog” Pete Hegseth
+Anthropic oplegt, namelijk het uitroepen tot “risico voor de toeleveringsketen”
+, creëert
+“een cultuur van dwang, medeplichtigheid en stilte waarbij het publiek begrijpt dat de
+regering alle middelen waarover het beschikt zal gebruiken om te straffen wie het
+aandurft het oneens te zijn”
+. En dat staat in een document dat – indirect – ondertekend is
+door Amazon, Google en Apple (via de belangengroep Chamber of Progress).</p>
+<p>Is dit nu het “Ik ben Spartacus!’” moment van Silicon Valley? Nee, daarvoor is het allemaal
+veel te voorzichtig. Chamber of Progress argumenteert vooral dat Anthropics recht op vrije
+meningsuiting wordt geschonden. Het is Anthropics goed recht om ethische bezwaren te
+formuleren, zeggen ze. Microsoft gaat, opmerkelijk, net iets verder: het spreekt zelfs
+voorzichtige steun uit voor die ethische bezwaren, met heel wat mitsen en maren.</p>
+<p>Opkomen voor het recht op vrije meningsuiting, daarmee steek je in de VS je nek niet te
+ver uit. Trump en zijn Maga-beweging hebben van dat begrip natuurlijk wel een heel eigen
+invulling: jaren hebben ze luid geklaagd dat big tech hun eigen meningsuiting beknot, nu
+verdragen ze amper tegenspraak. Maar in wat bredere conservatieve kringen is er best nog
+wel steun voor het idee dat iedereen zijn zegje mag hebben.</p>
+<p>Wanneer deze ceo’s zich in het verleden van hun progressiefste kant lieten zien, was dat
+(zoveel is ondertussen duidelijk) zelden vanuit een diepe overtuiging. Het was eerder
+omdat veel van hun waardevolste werknemers dat op prijs stellen. Zo kregen de
+werknemers van Google het bedrijf ooit zo ver dat het een uiterst lucratief contract met
+Defensie liet vallen.</p>
+<p>Tegenwoordig hebben de werknemers van Silicon Valley veel minder in de pap te brokken,
+en dat merk je. Met één uitzondering, weliswaar: de paar honderd AI-experts die de meest
+geavanceerde LLM’s kunnen bouwen. Die zijn zo in trek, en zo rijk, dat ze wél nog de luxe
+hebben om volgens hun overtuiging te handelen.</p>
+<p>Daarom is het waarschijnlijk geen toeval dat de technologiebedrijven net nu en in deze
+zaak beslissen hun rug te rechten: Amodei neemt heel nadrukkelijk en moedig een
+ethisch standpunt in over AI. Dat is bij die topontwikkelaars erg goed ontvangen.</p>
+
+
+      <ul className="list-disc pl-8 text-gray-700 space-y-2">
+LEES OOK
+<ul className="list-disc pl-5 text-gray-700 space-y-2"></ul>
+<li>ChatGPT boycotten, of meteen de hele big tech?</li>
+      </ul>
+       
+
+
+
+    </div>
+
+
+
+
+
+  )
+}
+
+
